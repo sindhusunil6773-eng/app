01b650c2247d7ff1fcae19569e8923032330c155
sindhusunil6773-eng <sindhusunil6773@gmail.com>
2026-06-18 19:51:19 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,7 @@
 import streamlit as st
-st.title("EXAM SCORE PREDICTOR")
-n=st.write("enter hrs stuided")
-st.number_input("hrs studied")
-st.button("predict")
+st.title("NIVEDITHA'S 1ST APP")
+n=st.write("HELLO")
+st.text_input("enter your name")
+st.number_input("enter your age")
+st.button("click me")
+st.success("SUCCESS")
